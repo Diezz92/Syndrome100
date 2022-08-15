ed0f718ab73692b3d8d0b499140457ad4e0cb56a
Станислав <diezz_92@mail.ru>
2022-08-15 16:55:33 +0500

    100 percent

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,11 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxInTheEnd() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 50};
+        long expected = 50;
 
         long actual = service.findMax(incomesInBillions);
 
